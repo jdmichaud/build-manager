a75891b527652a2d932d2c667e175fb2d152599e
jd <jean.daniel.michaud@gmail.com>
2017-12-01 20:05:54 +0100

    Add Dockerfile and do not use a password for ssh

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM alpine:latest
+
+RUN apk add --update --no-cache bash nodejs nodejs-npm
+
+RUN node --version
+
+RUN mkdir app
+ADD package.json /app
+ADD config.schema.json /app
+ADD server.js /app
+
+WORKDIR /app
+
+RUN npm install --production && \
+  chmod 700 server.js
+
+CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+const tmp = require('tmp');
 const githubhook = require('githubhook');
-const {validate} = require('jsonschema');
+const { validate } = require('jsonschema');
 const ssh = require('ssh-exec');
 
-const config = validate(require('./config.json'), require('./config.schema.json'), {throwError: true}).instance;
+const config = validate(
+  require('./config.json'),
+  require('./config.schema.json'),
+  { throwError: true }
+).instance;
 
 const handler = githubhook(config.hook);
 
 handler.on('push', (repos, ref) => {
   if (config.repositories.indexOf(repos) !== -1 && ref === 'refs/heads/master') {
-    const command = `cd /home/ubuntu/docker && docker-compose build --no-cache ${repos.toLowerCase()} && docker-compose up -d && docker system prune -f`;
+    const command = `
+      cd $DOCKER_COMPOSE_PATH &&
+      docker-compose build --no-cache ${repos.toLowerCase()} &&
+      docker-compose up -d &&
+      docker system prune -f`;
     ssh(command, config.ssh).pipe(process.stdout);
   }
 });
