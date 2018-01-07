9f8ac730bbc588e8eec5b8643dc6d5d2da588c75
jd <jean.daniel.michaud@gmail.com>
2018-01-07 13:52:52 +0100

    Add some debug to the execution of bitbucket as the ssh-exec failure message is useless.

--- server.js
+++ server.js
@@ -48,12 +48,14 @@ const bbhandler = bitbuckethook(config.hooks.bitbucket);
 
 bbhandler.on('push', (path, ref) => {
  if (config.hooks.bitbucket.repositories.indexOf(path) !== -1 && ref === 'master') {
+  console.log('received bitbucket call for repo ', path);
   // Because docker-compose is not able to use the ssh public key when clone
   // a private repo (https://github.com/docker/compose/issues/2856), we have
   // to clone the repo manually into /tmp.
   // We'll do this only for bitbucket, which hosts the private repos.
   repo = path.split('/')[1];
   const command = `
+    set -v
     rm -fr /tmp/${repo} &&
     git clone git@bitbucket.org:${path}.git /tmp/${repo}
     cd ${config.compose.path} &&
